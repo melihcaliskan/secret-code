fe5ffe5cdd8eedd03d0fefd8ac24259db825eb6d
Melih Çalışkan <melih.caliskan@tapu.com>
2022-03-02 00:07:06 +0300

    finish tasks

--- components/ShareButtons/ShareButtons.component.tsx
+++ components/ShareButtons/ShareButtons.component.tsx
@@ -1,17 +1,13 @@
+import { Stack } from "@chakra-ui/react";
+import Clipboard from "components/Clipboard/Clipboard.component";
+
 export function ShareButtons() {
   return (
-    null
-  )
-  
-  return (
-    <>
-      <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-show-count="false">Tweet</a>
-      <div className="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-size="small">
-        <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore" rel="noreferrer">
-          Share
-        </a>
-      </div>
-    </>
+    <Stack
+      w={"100%"}
+      mb={4}>
+      <Clipboard />
+    </Stack>
   )
 }
 
