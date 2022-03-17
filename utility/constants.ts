dd7c349bcb78671ddb3d02650af07d9f2d83d760
Melih Çalışkan <melih.caliskan@tapu.com>
2022-03-17 22:40:10 +0300

    add attempt count

--- utility/constants.ts
+++ utility/constants.ts
@@ -8,4 +8,4 @@ export const BOARD_SIZE = 4;
 export const BOARD_ROWS = 8;
 
 export const SUCCESS_MESSAGE = (count: number, playTime: number) => `You found the code in ${count} attempt${count > 1 ? `s`: ``} and ${playTime} seconds`;
-export const FAIL_MESSAGE = (count: number, playTime: number) => `You couldn't find the code in attempt${count > 1 ? `s` : ``} and ${playTime} seconds`;
+export const FAIL_MESSAGE = (count: number, playTime: number) => `You couldn't find the code in ${count} attempt${count > 1 ? `s` : ``} and ${playTime} seconds`;
